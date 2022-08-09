479b837a1d5955ac2ce1f6029a39627963617ccb
Putra1105 <87384137+Putra1105@users.noreply.github.com>
2022-08-09 17:46:15 +0700

    add Route

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Nav from '../component/Navbar/Nav.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Account from '../pages/Account.jsx';
+import Setting from '../pages/Setting.jsx';
+import Information from '../pages/Information.jsx';
 
 const Home = () => {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" />
+        <Route path="/" component={Home} />
+        <Route path="/account" component={Account} />
+        <Route path="/setting" component={Setting} />
+        <Route path="/information" component={Information} />
       </Routes>
     </Router>
   );
